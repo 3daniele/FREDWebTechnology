fb7daeb0d6f34660e96e7172efd7ba7f06c1bbc1
3daniele <dev.danieledd@gmail.com>
2021-08-06 12:20:57 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+< lang="en">
+<hea
+    eta charset="UTF-8">
+    eta http-equiv="X-UA-Compatible" content="IE=edge">
+    eta name="viewport" content="width=device-width, initial-scale=1.0">
+    ink rel="stylesheet" href="https://bootswatch.com/3/paper/bootstrap.css">
+  <tit
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="./">Brand</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="tendina">
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Nome <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Ordini</a></li>
+            <li><a href="#">Preferiti</a></li>
+            <li><a href="#">Supporto</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Account</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Accedi - Disconnetti</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Pannello di amministrazione</a></li>
+          </ul>
+        </li>
+        <li><a href="#">carrello</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container" id="main-area" style="margin-top: 70px;">
+    <div class="row">
+        <div class="col-9">
+
+        </div>
+        <div class="col-3">
+
+        </div>
+    </div>
+</div>
+
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Chi siamo</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Dove siamo</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Cookie Policy</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Privacy Policy</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contatti</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2017-2021 FRED</p>
+  </footer>
+
+<script data-cfasync="false" src="https://bootswatch.com/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="https://bootswatch.com/3/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://bootswatch.com/3/assets/js/custom.js"></script>
+</body>
+</html>
