1747988775956df48360026d028608849942d87a
3daniele <dev.danieledd@gmail.com>
2021-08-11 12:21:33 +0200

    login page

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ROOT_URL', 'http://localhost:80/FREDWebTechnology/');
+define('ROOT_URL', 'http://localhost:8888/FREDWebTechnology/');
 define('ROOT_PATH', 'cd /Applications/MAMP/htdocs/FREDWebTechnology/');
 
 define('DB_HOST', 'localhost');
