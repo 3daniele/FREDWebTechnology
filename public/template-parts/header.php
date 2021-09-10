9f263e77390c1ea3c9664bf078ebb3d77f45c744
3daniele <dev.danieledd@gmail.com>
2021-09-10 17:45:43 +0200

    new logo

--- public/template-parts/header.php
+++ public/template-parts/header.php
@@ -5,7 +5,7 @@
               <div class="container">
                   <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                          <li><a href=<?php echo ROOT_URL ?> class="nav-link px-2 link-secondary"><img src="<?php echo ROOT_URL ?>public/img/logo.jpeg" alt="BIO" width="100px"></img></a></li>
+                          <li><a href=<?php echo ROOT_URL ?> class="nav-link px-2 link-secondary"><img src="<?php echo ROOT_URL ?>public/img/logo.jpg" alt="BIO" width="100px"></img></a></li>
                       </ul>
 
                       <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action = "<?php echo ROOT_URL; ?>shop" method="GET">
