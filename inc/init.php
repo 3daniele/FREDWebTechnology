4b2ceba41407fff84fc8a5916b7860c8a20bce42
3daniele <dev.danieledd@gmail.com>
2022-01-14 18:12:12 +0100

    template support

--- inc/init.php
+++ inc/init.php
@@ -3,9 +3,11 @@
 session_start();
 
 require_once 'config.php';
-require_once ROOT_PATH . '/vendor/autoload.php';
+
 require_once ROOT_PATH . 'inc/globals.php';
 require_once ROOT_PATH . 'inc/functions.php';
+
+require_once ROOT_PATH . '/vendor/autoload.php';
 
 require_once ROOT_PATH . 'classes/DB.php';
 require_once ROOT_PATH . 'classes/User.php';
