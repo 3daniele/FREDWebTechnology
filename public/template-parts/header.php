e5dd45d690ef1b9f3f6210d4b342ad0ec9641d1e
3daniele <dev.danieledd@gmail.com>
2022-01-16 22:06:36 +0100

    new logo

--- public/template-parts/header.php
+++ public/template-parts/header.php
@@ -5,9 +5,8 @@
               <div class="container">
                   <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                          <li><a href=<?php echo ROOT_URL ?> class="nav-link px-2 link-secondary"><img src="<?php echo ROOT_URL ?>public/img/logo.jpg" alt="BIO" width="100px"></img></a></li>
+                          <li><a href=<?php echo ROOT_URL ?> class="nav-link px-2 link-secondary"><img src="<?php echo ROOT_URL ?>public/img/logo.jpg" alt="BIO" width="300px"></img></a></li>
                       </ul>
-
                       <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action = "<?php echo ROOT_URL; ?>shop" method="GET">
                           <input type="search" name="search" class="form-control" placeholder="Cerca..." aria-label="Search">
                       </form>
