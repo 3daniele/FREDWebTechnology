7ad40698a5ff37a0c18b6c71643a86dc2e2ed97d
3daniele <dev.danieledd@gmail.com>
2022-01-17 18:59:21 +0100

    nuova homepage

--- classes/ProductCategory.php
+++ classes/ProductCategory.php
@@ -4,15 +4,30 @@ class ProductCategoryManager extends DBManager
   public function __construct()
   {
     parent::__construct();
-    $this->columns = array('id', 'name', 'description', 'price', 'manufacturer_id','category');
+    $this->columns = array('id', 'name', 'description', 'small_description','price', 'manufacturer_id','category','link');
     $this->tableName = 'productcategory';
   }
 
   public function search($search)
   {
-    return $this->db->query("SELECT DISTINCT id, name, description, price FROM productcategory WHERE MATCH (name) AGAINST ('$search' IN NATURAL LANGUAGE MODE)
+    return $this->db->query("SELECT DISTINCT id, name, description, small_description, price FROM productcategory WHERE MATCH (name) AGAINST ('$search' IN NATURAL LANGUAGE MODE)
                              UNION SELECT DISTINCT id, name, description, price FROM productcategory WHERE MATCH (category) AGAINST ('$search' IN NATURAL LANGUAGE MODE)");
   }
+
+  public function getByCategory($categoryName){
+    return $this->db->query("SELECT *FROM productcategory WHERE category = '$categoryName'");
+  }
+
+  public function getNumberOf($categoryName){
+    return $this->db->query("SELECT COUNT(id) AS number FROM productcategory WHERE category = '$categoryName'");
+  }
+
+  public function getMostPurchased(){
+    return $this->db->query("SELECT DISTINCT id, name, description, small_description, price, img FROM productcategory ORDER BY price DESC LIMIT 14");
+  }
+
+  
+  
 }
 
 ?>
--- shop/index.php
+++ shop/index.php
@@ -12,6 +12,8 @@ $categoryItemMgr = new CategoryItemManager();
 $categoryMgr = new CategoryManager();
 $imgMgr = new ImgManager();
 $reviewMgr = new ReviewManager();
+$wishlistMgr = new WishlistManager();
+
 
 if (isset($_GET["search"])) {
     $search = $_GET["search"];
@@ -28,19 +30,24 @@ $imgs = $imgMgr->getAll();
 $categories = $categoryMgr->getAll();
 $categoryItems = $categoryItemMgr->getAll();
 $reviews = $reviewMgr->getAll();
-
-// var_dump($imgs[0]->thumbnail);
+$creme = $productCategoryMgr->getByCategory("creme");
+$cremeNumber = $productCategoryMgr->getNumberOf("creme");
+$most = $productCategoryMgr->getMostPurchased();
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader, []);
 
 echo $twig->render('index.html', [
+    'username' => $_SESSION['name'],
     'ROOT_URL' => ROOT_URL,
     'products' => $products,
     'imgs' => $imgs,
     'categories' => $categories,
     'categoryItems' => $categoryItems,
-    'reviews' => $reviews
+    'reviews' => $reviews,
+    'creme' => $creme,
+    'cremeNumber' => $cremeNumber[0]['number'],
+    'most' => $most
 ]);
 
 include ROOT_PATH . 'public/template-parts/footer.php';
