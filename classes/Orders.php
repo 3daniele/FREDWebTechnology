3c315dd4bf2e72fff44bd6ebdcb8cd22c4ee3fe6
domenico597 <domenicobonali@gmail.com>
2022-01-18 19:14:13 +0100

    wishlist e orders

--- classes/Orders.php
+++ classes/Orders.php
@@ -1,14 +1,17 @@
 <?php
 
-class OrdersManager extends DbManager {
+class OrdersManager extends DbManager
+{
 
-  public function __construct(){
+  public function __construct()
+  {
     parent::__construct();
-    $this->columns = array('id', 'user_id', 'date_order', 'status','tracking_information', 'stimate_delivery', 'shipment_user_info');
+    $this->columns = array('id', 'user_id', 'date_order', 'status', 'tracking_information', 'stimate_delivery', 'shipment_user_info');
     $this->tableName = 'Orders';
   }
 
-  public function addOrder($userID, $shipmentID) {
+  public function addOrder($userID, $shipmentID)
+  {
     $d = strtotime("+7 days");
     $stimate_delivery = date("Y-m-d", $d);
 
@@ -16,29 +19,33 @@ class OrdersManager extends DbManager {
     return $this->getLastOrder($userID, $shipmentID);
   }
 
-  private function getLastOrder($userID, $shipmentID) {
+  private function getLastOrder($userID, $shipmentID)
+  {
     $result = $this->db->query("SELECT * FROM Orders WHERE user_id = $userID AND shipment_user_info = $shipmentID ORDER BY id DESC LIMIT 1");
     return $result[0]['id'];
   }
-  
-  public function getsum($idOrder){
-    $result=$this->db->query(" SELECT SUM(Product.price*Orders_items.quantity) AS totale FROM Orders, Orders_items, Product WHERE Orders.id = $idOrder AND Orders.id = Orders_items.order_id AND Orders_items.product_id = Product.id ");
+
+  public function getsum($idOrder)
+  {
+    $result = $this->db->query(" SELECT SUM(Product.price*Orders_items.quantity) AS totale FROM Orders, Orders_items, Product WHERE Orders.id = $idOrder AND Orders.id = Orders_items.order_id AND Orders_items.product_id = Product.id ");
     return $result;
   }
-  
-  public function getOrder($userID) {
+
+  public function getOrder($userID)
+  {
     $result = $this->db->query("SELECT * FROM Orders WHERE user_id = $userID ");
     return $result;
   }
 
-  public function getOrderByID($orderID) {
+  public function getOrderByID($orderID)
+  {
     $result = $this->db->query("SELECT * FROM Orders WHERE id = $orderID ");
     return $result[0];
   }
   /* DASHBOARD */
 
-  public function getLastOrders(){
-    return $this->db->query("SELECT * FROM Orders ORDER BY id DESC LIMIT 3");                                 
+  public function getLastOrders()
+  {
+    return $this->db->query("SELECT * FROM Orders ORDER BY id DESC LIMIT 3");
   }
-
 }
