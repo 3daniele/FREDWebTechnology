1bb879c205f14963ba26be0a69b4f749c8bb11dd
3daniele <dev.danieledd@gmail.com>
2022-01-18 19:15:40 +0100

    template admin

--- admin/index.php
+++ admin/index.php
@@ -13,256 +13,51 @@
 
 <?php include ROOT_PATH . "admin/pages/template-parts/adminSidebar.php"; ?>
 
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h2><strong class="text-primary">Dashboard</strong></h2>
-    </div>
-    <div class="row">
-        <div class="col-md-6 col-lg-7">
-            <div class="card">
-                <div class="card-header">
-
-                    <span class="text-primary">
-                        <div class="row">
-                            <div class="col-9">
-                                <h4><b>Ultimi ordini:</b></h4>
-                            </div>
-                            <div class="col-3 text-end">
-                                <a href="<?php echo ROOT_URL . 'admin/pages/order.php'; ?>" class="btn btn-primary btn-sm">Gestisci &raquo;</a>
-                            </div>
-                        </div>
-                    </span>
+<?php 
+    $orderMgr = new OrdersManager();
+    $orderAddressMgr = new OrderAddressManager();
+    $userMgr = new UserManager();
+    $supportMgr = new SupportManager();
+    $reviewMgr = new ReviewManager();
+    $productMgr = new ProductManager();
 
 
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-1">
-                            <b> #</b>
-                        </div>
-                        <div class="col-2">
-                            <b>Data</b>
-                        </div>
-                        <div class="col-2">
-                            <b>Stato</b>
-                        </div>
-                        <div class="col-2">
-                            <b>Utente</b>
-                        </div>
-                        <div class="col-5">
-                            <b>Indirizzo</b>
-                        </div>
-                    </div>
-                    <hr>
-                    <?php
-                    $orderMgr = new OrdersManager();
-                    $orders = $orderMgr->getLastOrders();
-                    $i = 0;
-                    foreach ($orders as $order) : $i++;
-                    ?>
+    $orders = $orderMgr->getLastOrders();
+    $users = array();
+    $addresses = array();
 
-                        <div class="row">
-                            <p class="card-text">
-                            <div class="col-1">
-                                <?php echo $order['id']; ?>
-                            </div>
-                            <div class="col-2">
-                                <?php echo $order['date_order']; ?>
-                            </div>
-                            <div class="col-2">
-                                <?php echo $order['status']; ?>
-                            </div>
-                            <div class="col-2">
-                                <?php
-                                $userMgr = new UserManager();
-                                $users = $userMgr->getName($order['user_id']);
-                                echo $users[0]['name'] . ", ";
-                                $users = $userMgr->getSurname($order['user_id']);
-                                echo $users[0]['surname'];
-                                ?>
-                            </div>
-                            <div class="col-5">
-                                <?php
-                                $orderAddressMgr = new OrderAddressManager();
-                                $addressinfo = $orderAddressMgr->getAddressByOrder($order['id']);
-                                $string = $addressinfo[0]['address'] . ", " . $addressinfo[0]['city_name'] . ", " . $addressinfo[0]['code'] . ", " . $addressinfo[0]['provinces_name'] . ".";
-                                echo $string; ?>
-                            </div>
-                            </p>
-                        </div>
-                        <?php if ($i != count($orders)) : ?>
-                            <hr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-            <hr>
+    foreach($orders as $order){
+        $user = $userMgr->get($order['user_id']);
+        $addressinfo = $orderAddressMgr->getAddressByOrder($order['id']);
+        $string = $addressinfo['address'] . ", " . $addressinfo['city_name'] . ", " . $addressinfo['code'] . ", " . $addressinfo['provinces_name'] . ".";
+        $string2 = $user->name.", ".$user->surname;
+        
+        array_push($addresses, $string);
+        array_push($users, $string2);
+    }
 
-            <div class="card">
-                <div class="card-header">
-                    <span class="text-primary">
-                        <div class="row">
-                            <div class="col-9">
-                                <h4><b>Ticket aperti:</b></h4>
-                            </div>
-                            <div class="col-3 text-end">
-                                <a href="<?php echo ROOT_URL . 'admin/pages/support/support.php'; ?>" class="btn btn-primary btn-sm">Gestisci &raquo;</a>
-                            </div>
-                        </div>
-                    </span>
+    $tickets = $supportMgr->showOpenTicket();
+
+    $reviews = $reviewMgr->getLastReview();
+
+    $products = $productMgr->getEsaurimenti();
+    
+    $loader = new \Twig\Loader\FilesystemLoader(ROOT_PATH . '/templates/admin/');
+    $twig = new \Twig\Environment($loader, []);
+    
+    echo $twig->render('index.html', [
+        'ROOT_URL' => ROOT_URL,
+        'orders' => $orders,
+        'users' => $users,
+        'addresses' => $addresses,
+        'tickets' => $tickets,
+        'reviews' => $reviews,
+        'products' => $products
+        
+    ]);
 
 
-                </div>
-                <div class="card-body">
-                    <?php $supportMgr = new SupportManager();
-                    $tickets = $supportMgr->showOpenTicket();
-                    $i = 0;
-                    foreach ($tickets as $ticket) : $i++;
-                    ?>
-                        <p class="card-text"><strong><?php echo $ticket['object'] ?></strong><br><?php echo $ticket['message'] ?></p>
-                        <?php if ($i < 3) : ?>
-                            <hr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-            <hr>
-
-            <div class="card">
-                <div class="card-header">
-
-                    <span class="text-primary">
-                        <div class="row">
-                            <div class="col-9">
-                                <h4><b>Nuove recensioni:</b></h4>
-                            </div>
-                            <div class="col-3 text-end">
-                                <a href="<?php echo ROOT_URL . 'admin/pages/review/review.php'; ?>" class="btn btn-primary btn-sm">Gestisci &raquo;</a>
-                            </div>
-                        </div>
-                    </span>
-
-
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-1">
-                            <b><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-half" viewBox="0 0 16 16">
-                                    <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
-                                </svg></b>
-                        </div>
-                        <div class="col-2">
-                            <b>Titolo</b>
-                        </div>
-                        <div class="col-5">
-                            <b>Descrizione</b>
-                        </div>
-                        <div class="col-2">
-                            <b>Prodotto</b>
-                        </div>
-                        <div class="col-2">
-                            <b>Utente</b>
-                        </div>
-                    </div>
-                    <hr>
-                    <?php $reviewMgr = new ReviewManager();
-                    $reviews = $reviewMgr->getLastReview();
-                    $i = 0;
-                    foreach ($reviews as $review) : $i++;
-                    ?>
-                        <div class="row">
-                            <p class="card-text">
-                            <div class="col-1">
-                                <?php echo $review['vote']; ?>
-                            </div>
-                            <div class="col-2">
-                                <?php echo $review['title']; ?>
-                            </div>
-                            <div class="col-5">
-                                <?php echo $review['message']; ?>
-                            </div>
-                            <div class="col-2">
-                                <?php echo $review['product']; ?>
-                            </div>
-                            <div class="col-2">
-                                <?php echo $review['user']; ?>
-                            </div>
-                            </p>
-                        </div>
-                        <?php if ($i != count($reviews)) : ?>
-                            <hr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-
-
-                </div>
-            </div>
-            <hr>
-
-        </div>
-        <div class="col-md-4 col-lg-4 px-md-4">
-            <div class="card">
-                <div class="card-header">
-
-                    <span class="text-primary">
-                        <div class="row">
-                            <div class="col-7">
-                                <h4><b>In esaurimento:</b></h4>
-                            </div>
-                            <div class="col-5 text-end">
-                                <a href="<?php echo ROOT_URL . 'admin/pages/product/product.php'; ?>" class="btn btn-primary btn-sm">Gestisci &raquo;</a>
-                            </div>
-                        </div>
-                    </span>
-
-
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-7">
-                            <b>Prodotto</b>
-                        </div>
-                        <div class="col-2"></div>
-                        <div class="col-3 text-end">
-                            <b>Stock</b>
-                        </div>
-                    </div>
-                    <hr>
-                    <?php $productMgr = new ProductManager();
-                    $products = $productMgr->getEsaurimenti();
-                    $i = 0;
-                    foreach ($products as $product) : $i++; ?>
-                        <div class="row">
-                            <p class="card-text">
-                            <div class="col-7"><?php echo $product['name']; ?></div>
-                            <div class="col-2"></div>
-                            <?php if ($product['stock'] <= 5) : ?>
-                                <div class="col-3 text-end text-danger">
-                                <?php else : ?>
-                                    <div class="col-3 text-warning text-end">
-                                    <?php endif; ?>
-                                    <?php
-                                    echo ("<b>");
-                                    echo $product['stock'];
-                                    echo ("</b></span>");
-                                    ?>
-                                    </div>
-                                    </p>
-                                    <?php if ($i != count($products)) : ?>
-                                        <hr>
-                                    <?php endif; ?>
-                                </div>
-                            <?php endforeach; ?>
-
-                        </div>
-                </div>
-
-
-            </div>
-
-        </div>
-</main>
-
+?>
 
 
 <?php include ROOT_PATH . "public/template-parts/footer.php"; ?>
--- admin/pages/client.php
+++ admin/pages/client.php
@@ -13,84 +13,30 @@
 
 <?php include ROOT_PATH . "admin/pages/template-parts/adminSidebar.php"; ?>
 
-<main class="col-md-9 col-lg-8 px-md-4">
-    <div class="row border-bottom">
-        <div class="col-4">
-            <h2><strong class="text-primary">Clienti</strong></h2>
-        </div>
-        <div class="col-4"></div>
-        <div class="col-2 text-end"></div>
-        <div class="col-2 text-end">
-            <form>
-                <input type="search" class="form-control" placeholder="Cerca utente" aria-label="Search">
-            </form>
-        </div>
-    </div>
-    <div class="card" style="border: none;">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-2">
-                    <h5><b></b></h5>
-                </div>
-                <div class="col-2">
-                    <h5><b>Nome</b></h5>
-                </div>
-                <div class="col-2">
-                    <h5><b>Cognome</b></h5>
-                </div>
-                <div class="col-2">
-                    <h5><b>Email</b></h5>
-                </div>
-                <div class="col-2">
-                    <h5><b>Tipo</b></h5>
-                </div>
-                <div class="col-2 text-end">
-                    <h5><b>Azione</b></h5>
-                </div>
-            </div>
-            <hr>
-            <?php
-            $userMgr = new UserManager();
-            $users = $userMgr->getClient();
-            $chatMgr = new ChatManager();
-            ?>
-            <?php foreach ($users as $user) : ?>
-            <div class="row">
-                <p class="card-text">
-                <div class="col-2">
-                    <img src="<?php echo ROOT_URL . $user['img'] ?>" width=100px height=100px class="rounded-circle">
-                </div>
-                <div class="col-2">
-                    <?php echo $user['name']; ?>
-                </div>
-                <div class="col-2">
-                    <?php echo $user['surname']; ?>
-                </div>
-                <div class="col-2">
-                    <?php echo $user['email']; ?>
-                </div>
-                <div class="col-2">
-                    <?php if($user['user_type']==1) :?>
-                    Utente
-                    <?php else :?>
-                    Admin
-                    <?php endif;?>
-                </div>
-                <div class="col-2 text-end">
-                    <?php $chat = $chatMgr->getUserChat($user['id']);
-                            $chatid=$chat[0]['id'];
-                    ?>
-                    <a class="btn btn-primary" href="<?php echo ROOT_URL.'admin/pages/message/message.php?chat='.$chatid ?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chat-dots-fill" viewBox="0 0 16 16">
-                                <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                                 </svg> Chat</a>
-                </div>
-                </p>
-            </div>
-            <hr>
-            <?php endforeach; ?>
-        </div>
-</main>
-<div class="col-lg-1 px-md-4"></div>
+<?php
+    $userMgr = new UserManager();
+    $chatMgr = new ChatManager();
+
+    $users = $userMgr->getClient();
+    
+    $chatf = array();
+
+    foreach($users as $user){
+        $chat = $chatMgr->getUserChat($user['id']);
+        $chatid=$chat[0]['id'];
+        array_push($chatf,$chatid);
+    }
+
+$loader = new \Twig\Loader\FilesystemLoader(ROOT_PATH . '/templates/admin/');
+    $twig = new \Twig\Environment($loader, []);
+    
+    echo $twig->render('client.html', [
+        'ROOT_URL' => ROOT_URL,
+        'users' => $users,
+        'chat' => $chatf
+        
+    ]);
+?>
 
 
 
