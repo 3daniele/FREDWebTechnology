e51097ea3bb4c09efa904a1ae32744ac4634efd9
domenico597 <domenicobonali@gmail.com>
2022-01-19 18:34:51 +0100

    indirizzi e pagamenti

--- user/address.php
+++ user/address.php
@@ -1,21 +1,14 @@
 <?php
 include "../inc/init.php";
+
 if (!isset($_SESSION["email"])) {
     header("Location: " . ROOT_URL);
 }
-?>
 
-<?php
 include ROOT_PATH . "public/template-parts/title.php";
-?>
-<script type="text/javascript" src="<?php echo ROOT_URL; ?>assets/js/address.js"></script>
-<title>Indirizzi</title>
 
-<?php
 include ROOT_PATH . "public/template-parts/header.php";
-?>
 
-<?php
 $shipIMgr = new ShipmentInformationManager();
 $addressMgr = new AddressManager();
 $regionMgr =  new RegionManager();
@@ -24,132 +17,21 @@ $cityMgr = new CityManager();
 
 $indirizzi = $shipIMgr->getIndirizzi($_SESSION['userid']);
 
-$addresses = $addressMgr->getAll();
-$regions = $regionMgr->getAll();
-$provinces = $provinceMgr->getAll();
-$cities = $cityMgr->getAll();
-?>
+$addresses = json_encode($addressMgr->getAll());
+$regions = json_encode($regionMgr->getAll());
+$provinces = json_encode($provinceMgr->getAll());
+$cities = json_encode($cityMgr->getAll());
 
-<span id="flagRegion" hidden>0</span>
-<span id="flagProvince" hidden>0</span>
-<span id="flagCity" hidden>0</span>
+$loader = new \Twig\Loader\FilesystemLoader(ROOT_PATH . 'templates/user');
+$twig = new \Twig\Environment($loader, []);
 
-<span id="addresses" hidden><?php echo json_encode($addresses) ?></span>
-<span id="regions" hidden><?php echo json_encode($regions) ?></span>
-<span id="provinces" hidden><?php echo json_encode($provinces) ?></span>
-<span id="cities" hidden><?php echo json_encode($cities) ?></span>
+echo $twig->render('address.html', [
+    'ROOT_URL' => ROOT_URL,
+    'addresses' => $addresses,
+    'regions' => $regions,
+    'provinces' => $provinces,
+    'cities' => $cities,
+    'indirizzi' => $indirizzi
+]);
 
-<div class="container" id="main-area" style="margin-top: 70px;">
-    <div class="row">
-        <?php if ($indirizzi) : ?>
-            <?php foreach ($indirizzi as $indirizzo) : ?>
-                <div class="col-3">
-                    <div class="card mb-3">
-                        <h3 class="card-header text-center">Indirizzo <?php if ($indirizzo['principal'] == 1) echo " predefinito" ?> </h3>
-                        <li class="list-group-item"> <?php echo $indirizzo['address'] . "<br>" . $indirizzo['city_name'] . ", " . $indirizzo['code'] . "<br>" . $indirizzo['provinces_name'] . "<br>";  ?></li>
-                        <div class="card-body text-center">
-                            <form method="POST" action="./address-action.php">
-                                <input class="form-control" id="shipmentID" name="shipmentID" type="text" hidden value="<?php echo $indirizzo['id_shipment']; ?>">
-                                <button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal" data-whatever="modifica" data-bs-whatever=<?php echo $indirizzo['id_shipment']; ?>>Modifica</button>
-                                <button class="btn btn-outline-danger btn-sm" name="delete" type="submit" value="<?php echo $indirizzo['id_shipment']; ?>">Elimina</button>
-                                <div class="row"><br></div>
-                                <?php if ($indirizzo['principal'] == 0) : ?>
-                                    <button class="btn btn-outline-success btn-sm" name="principal" type="submit" value=1>Rendi indirizzo predefinito</button>
-                                <?php else : ?>
-                                    <button class="btn btn-outline-success btn-sm" name="principal" type="submit" value=0>Rimuovi indirizzo predefinito</button>
-                                <?php endif; ?>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <p>Nessun indirizzo presente...</p>
-        <?php endif; ?>
-    </div>
-</div>
-<div class="container" id="main-area" style="margin-top: 25px; ">
-    <form action="./address-action.php" method="POST">
-        <div class="row">
-            <div class="col-2">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal" data-whatever="aggiungi">
-                    Aggiungi nuovo indirizzo
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
-
-<?php include ROOT_PATH . "public/template-parts/footer.php"; ?>
-
-<!-- Finestra di dialogo per l'aggiunta di un nuovo indirizzzo -->
-<!-- Modal -->
-<form method="POST" action="./address-action.php">
-    <div class="modal" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="text-primary"><strong id="modalTitle">Inserisci un nuovo indirizzo</strong></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="POST" action="./address-action.php">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="form-label">Regione:</label><br>
-                                        <select class="form-select" id="regione" name="regione">
-                                            <option selected>Seleziona la regione</option>
-                                            <?php foreach ($regions as $region) : ?>
-                                                <option value=<?php echo $region->id ?>><?php echo $region->name ?></option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="form-label" for="disabledInput">Provincia:</label><br>
-                                        <select class="form-select" id="provincia" name="provincia" disabled>
-                                            <option selected>Seleziona la provincia</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="form-label">Comune:</label><br>
-                                        <select class="form-select" id="comune" name="comune" disabled>
-                                            <option selected>Seleziona il comune</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="form-label" for="disabledInput">CAP:</label><br>
-                                        <input class="form-control" id="cap" name="cap" type="text" maxlength="5" placeholder="CAP" disabled>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="form-label" for="disabledInput">Via/Piazza e numero civico:</label><br>
-                                        <input class="form-control" id="address" name="address" type="text" placeholder="Via..." disabled>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" id="modalBtn" class="btn btn-primary" name="add">Invia</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
+include ROOT_PATH . "public/template-parts/footer.php";
--- user/payment.php
+++ user/payment.php
@@ -1,116 +1,25 @@
 <?php
 include "../inc/init.php";
+
 if (!isset($_SESSION["email"])) {
     header("Location: " . ROOT_URL);
 }
-?>
 
-<?php
 include ROOT_PATH . "public/template-parts/title.php";
-?>
-<script type="text/javascript" src="<?php echo ROOT_URL; ?>assets/js/payment.js"></script>
-<title>Informazioni pagamento</title>
 
-<?php
 include ROOT_PATH . "public/template-parts/header.php";
-?>
-
-<?php 
 
 $userID = $_SESSION['userid'];
 $paymentsMgr = new PaymentsManager();
 $payments = $paymentsMgr->getPayments($userID);
 
-?>
+$loader = new \Twig\Loader\FilesystemLoader(ROOT_PATH . 'templates/user');
+$twig = new \Twig\Environment($loader, []);
 
-<div class="container" id="main-area" style="margin-top: 70px;">
-    <div class="row">
-        <?php if ($payments) : ?>
-            <?php foreach ($payments as $payment) : ?>
-                <div class="col-3">
-                    <div class="card mb-3">
-                        <h3 class="card-header text-center">Metodo <?php if ($payment['principal'] == 1) echo " predefinito" ?></h3>
-                        <li class="list-group-item"> Numero di carta: <?php echo $payment['credit_card_number'] ?></li>
-                        <li class="list-group-item"> Scadenza: <?php echo $payment['expiration1'] . "/" . $payment['expiration2']?></li>
-                        <li class="list-group-item"> Cvv: <?php echo $payment['cvv'] ?></li>
-                        <div class="card-body text-center">
-                            <form method="POST" action="./payment-action.php">
-                                <input class="form-control" id="paymentID" name="paymentID" type="text" hidden value="<?php echo $payment['id']; ?>">
-                                <?php if ($payment['principal'] == 0) : ?>
-                                    <button class="btn btn-outline-success btn-sm" name="principal" type="submit" value=1>Rendi metodo predefinito</button>
-                                <?php else : ?>
-                                    <button class="btn btn-outline-success btn-sm" name="principal" type="submit" value=0>Rimuovi metodo predefinito</button>
-                                <?php endif; ?>                                
-                                <button class="btn btn-outline-danger btn-sm" name="delete" type="submit" value="<?php echo $payment['id']; ?>">Elimina</button>                                
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <p>Nessun metodo di pagamento presente...</p>
-        <?php endif; ?>
-    </div>
-</div>
-<div class="container" id="main-area" style="margin-top: 25px; ">
-    <form action="./payment-action.php" method="POST">
-        <div class="row">
-            <div class="col-2">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal" data-whatever="aggiungi">
-                    Aggiungi nuovo metodo
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
+echo $twig->render('payment.html', [
+    'ROOT_URL' => ROOT_URL,
+    'userID' => $userID,
+    'payments' => $payments
+]);
 
-<?php include ROOT_PATH . "public/template-parts/footer.php"; ?>
-
-<!-- Finestra di dialogo per l'aggiunta di un nuovo indirizzzo -->
-<!-- Modal -->
-<form method="POST" action="./payment-action.php">
-    <div class="modal" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="text-primary"><strong id="modalTitle">Inserisci un nuovo metodo di pagamento</strong></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="POST" action="./payment-action.php">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <label class="form-label">Numero di carta:</label><br>
-                                        <input class="form-control" id="credit_card_number" name="credit_card_number" type="text" placeholder="1234xxx">
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-5">
-                                        <label class="form-label">Scadenza:</label><br>
-                                        <input class="form-control" id="expiration" name="expiration" type="text" placeholder="05/22">
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-3">
-                                        <label class="form-label" for="disabledInput">Cvv:</label><br>
-                                        <input class="form-control" id="cvv" name="cvv" type="text" placeholder="123">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" id="modalBtn" class="btn btn-primary" name="add">Invia</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
+include ROOT_PATH . "public/template-parts/footer.php";
