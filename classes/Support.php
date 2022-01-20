d7271d121f6e82affec188bcb819f4b0c9580575
3daniele <dev.danieledd@gmail.com>
2022-01-20 23:53:38 +0100

    pagina di supporto per utente

--- classes/Support.php
+++ classes/Support.php
@@ -8,6 +8,29 @@ class SupportManager extends DbManager {
     $this->tableName = 'Support';
   }
 
+  public function getOpenTicketUser($userId){
+    return $this->db->query("SELECT *FROM Support WHERE Support.status=0 AND user_id = '$userId' ORDER BY Support.date DESC");
+  }
+
+  public function getCloseTicketUser($userId){
+    return $this->db->query("SELECT *FROM Support WHERE Support.status=1 AND user_id = '$userId' ORDER BY Support.date DESC");
+  }
+
+  public function addSupport($userID, $object, $message)
+  {
+    return $this->db->execute("INSERT INTO Support (user_id, object, message) VALUES ($userID, '$object', '$message')");
+  }
+
+  public function addSupportOrder($userID, $object, $message, $orderid)
+  {
+    return $this->db->execute("INSERT INTO Support (user_id, object, message, order_id) VALUES ($userID, '$object', '$message', $orderid)");
+  }
+
+  public function getLast($userid)
+  {
+    return $this->db->query("SELECT *FROM Support WHERE user_id = '$userid' ORDER BY Support.date DESC LIMIT 1");
+  }
+
 
   /* DASHBOARD */
   public function showOpenTicket(){
