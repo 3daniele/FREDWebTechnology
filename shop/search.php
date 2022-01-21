18697ec28ef5891a43c0fbbf7a14f7c4107937c1
domenico597 <domenicobonali@gmail.com>
2022-01-21 21:42:08 +0100

    bug fix

--- shop/search.php
+++ shop/search.php
@@ -19,7 +19,7 @@ if (isset($_POST["selezionato"])) {
     $all = $_POST["selezionato"];
     foreach ($all as $i)
         array_push($categorie, $i);
-        $search = $_POST['search'];
+    $search = $_POST['search'];
 }
 
 $categoryMgr = new CategoryManager();
@@ -27,41 +27,38 @@ $categoryMgr = new CategoryManager();
 $category = $categoryMgr->getAll();
 
 $productCategoryMgr = new ProductCategoryManager();
-$products = $productCategoryMgr->search($search);//ricerca tramite nome cercato
+$products = $productCategoryMgr->search($search); //ricerca tramite nome cercato
 
 $provvisori = array();
-foreach($categorie as $cat){
-    array_push($provvisori,$productCategoryMgr->search($cat));
+foreach ($categorie as $cat) {
+    array_push($provvisori, $productCategoryMgr->search($cat));
 }
-var_dump($provvisori[0][0]);
 
 
-/*
-foreach($provvisori as $p){
-    if(!in_array($p, $products)){
-        array_push($products,$p);
-    }    
+foreach ($provvisori as $p) {
+    foreach ($p as $a) {
+        if (!in_array($a, $products)) {
+            array_push($products, $a);
+        }
+    }
 }
-*/
+
 
 $imgMgr = new ImgManager();
 $img = array();
 
+$loader = new \Twig\Loader\FilesystemLoader('../templates/shop');
+$twig = new \Twig\Environment($loader, []);
 
+echo $twig->render('search.html', [
+    'ROOT_URL' => ROOT_URL,
+    'search' => $search,
+    'product' => $products,
+    'selected' => $categorie,
+    'category' => $category,
+    //'provvisori' => $provvisori,
+    'redirect' => ROOT_URL . "shop/single-product.php?product=",
+    'REQUEST_URI' => $_SERVER['REQUEST_URI']
+]);
 
-    $loader = new \Twig\Loader\FilesystemLoader('../templates/shop');
-    $twig = new \Twig\Environment($loader, []);
-
-    echo $twig->render('search.html', [
-        'ROOT_URL' => ROOT_URL,
-        'search' => $search,
-        'product' => $products,
-        'selected' => $categorie,
-        'category' => $category,
-        'provvisori' => $provvisori,
-        'redirect' => ROOT_URL . "shop/single-product.php?product=",
-        'REQUEST_URI' => $_SERVER['REQUEST_URI']
-    ]);
-
-    include ROOT_PATH . 'public/template-parts/footer.php';
-
+include ROOT_PATH . 'public/template-parts/footer.php';
