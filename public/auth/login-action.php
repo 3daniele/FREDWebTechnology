e9b75c9b80f04965dc7f66bc1d0b968b46521c32
domenico597 <domenicobonali@gmail.com>
2022-01-21 17:40:20 +0100

    bug fix

--- public/auth/login-action.php
+++ public/auth/login-action.php
@@ -5,18 +5,30 @@ if (isset($_SESSION["email"])) {
     header("Location: " . ROOT_URL);
 }
 
+include ROOT_PATH . "public/template-parts/title.php";
+
+include ROOT_PATH . "public/template-parts/header.php";
+
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-$passHash = hash("sha1",$password);
+$passHash = hash("sha1", $password);
 
 $userMgr = new UserManager();
 $login = $userMgr->login($email, $passHash);
 
 
 if ($login == null) {
-    //ALERT
-}else{
+    $error = true;
+
+    $loader = new \Twig\Loader\FilesystemLoader(ROOT_PATH . 'templates/public/auth');
+    $twig = new \Twig\Environment($loader, []);
+    
+    echo $twig->render('login.html', [
+        'ROOT_URL' => ROOT_URL,
+        'error' => $error,
+    ]);
+} else {
     $_SESSION["email"] = $email;
     foreach ($login as $log) {
         $_SESSION["img"] = $log["img"];
@@ -26,3 +38,5 @@ if ($login == null) {
     }
     header("Location: " . ROOT_URL . "shop");
 }
+
+include ROOT_PATH . "public/template-parts/footer.php";
