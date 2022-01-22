3282cede9a99756a39432ad9b25c1a436dd343fb
domenico597 <domenicobonali@gmail.com>
2022-01-22 18:09:52 +0100

    inizio upload img prodotti

--- admin/pages/product/save-product.php
+++ admin/pages/product/save-product.php
@@ -1,24 +1,68 @@
-<?php include "../../../inc/init.php";?>
+<?php include "../../../inc/init.php"; ?>
 <?php if (!isset($_SESSION["email"]) || $_SESSION["admin"] == 1) {
     header("Location: " . ROOT_URL);
 }
 ?>
 <?php
-    $name = $_POST["name"];
-    
-    $description = (string)$_POST["description"];
-    
-    $small_description = (string)$_POST["small_description"];
-    
-    $price = $_POST["price"];
-    
-    $stock = (int)$_POST["stock"];
-    
-    $id = (int)$_POST["id"];
+$name = $_POST["name"];
 
-    $productMgr = new ProductManager();
-    $productMgr->updateProduct($id,$name,$description,$small_description,$price,$stock);
+$description = (string)$_POST["description"];
 
-    header("Location: ".ROOT_URL."admin/pages/product/edit-product.php?product=".$id);
-    
+$small_description = (string)$_POST["small_description"];
+
+$price = $_POST["price"];
+
+$stock = (int)$_POST["stock"];
+
+$id = (int)$_POST["id"];
+
+$productMgr = new ProductManager();
+$productMgr->updateProduct($id, $name, $description, $small_description, $price, $stock);
+
+$imgMgr = new ImgManager();
+$imgs = $imgMgr->getAllImgForAdmin($id);
+
+if (isset($_POST['add'])) {
+    $number = count($imgs) + 1;
+
+    $imgMgr->addImg($id);
+    $imgID = $imgMgr->getLast();
+    $imgMgr->updateImg($imgID['id'], $id, $number);
+}
+
+// upload immagine
+if (isset($_FILES)) {
+
+    if (isset($_POST['edit'])) {
+        $imgID = $_POST['edit'];
+
+        foreach ($imgs as $img) {
+            if ($img['id'] == $imgID) {
+                $link = $img['link'];
+            }
+        }
+
+        echo $link;
+        echo "<br>";
+
+        $tmp = $_FILES['image']['tmp_name'];
+        var_dump($_FILES['image']);
+        echo "<br>";
+        
+        if (is_uploaded_file($tmp)) {
+            $uploadDir = ROOT_PATH . $link; //aggiungo il folder di destinazione
+            echo $uploadDir;
+            echo "<br>";
+            if (move_uploaded_file($tmp, $uploadDir)) {
+                // OK
+                echo $uploadDir;
+            } else {
+                echo "Non è stato possibile caricare l'immagine<br/>";
+            }
+        }
+    }
+}
+
+//header("Location: " . ROOT_URL . "admin/pages/product/edit-product.php?product=" . $id);
+
 ?>
